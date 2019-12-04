043c1cda8c8e7d230092855e5e1195a10ddf6c10
agourrag <gourragui.success@gmail.com>
2019-12-04 11:50:13 +0100

    wip

--- ft_lstnew_bonus.c
+++ ft_lstnew_bonus.c
@@ -6,7 +6,7 @@
 /*   By: agourrag <agourrag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/29 17:53:12 by agourrag          #+#    #+#             */
-/*   Updated: 2019/12/04 08:52:41 by agourrag         ###   ########.fr       */
+/*   Updated: 2019/12/04 11:50:00 by agourrag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ t_list	*ft_lstnew(void *content)
 		res->content = NULL;
 	else
 		res->content = content;
-
 	res->next = NULL;
 	return (res);
 }
