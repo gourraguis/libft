7416d9d770b87bb10b9ed53747992669432675d9
agourrag <gourragui.success@gmail.com>
2019-12-08 13:25:41 +0100

    fix strlcat segfault

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,11 +6,13 @@
 /*   By: agourrag <agourrag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/17 08:40:02 by agourrag          #+#    #+#             */
-/*   Updated: 2019/12/02 07:47:32 by agourrag         ###   ########.fr       */
+/*   Updated: 2019/12/08 13:23:36 by agourrag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <string.h>
+#include <stdio.h>
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
@@ -18,10 +20,11 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	size_t n2;
 
 	n1 = ft_strlen(src);
-	n2 = ft_strlen(dst);
-	if (n2 > size)
-		n2 = size;
-	if (n2 == size)
+	if (!size)
+		n2 = 0;
+	else
+		n2 = ft_strlen(dst);
+	if (n2 >= size)
 		return (size + n1);
 	if (n1 + n2 < size)
 		ft_memcpy(dst + n2, src, n1 + 1);
